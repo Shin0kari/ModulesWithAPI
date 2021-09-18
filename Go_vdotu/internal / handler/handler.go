1562e5425a97e2ee2bfdb5458916fb978b3255dd
Shinokari <kasper13122@mail.ru>
2021-09-18 15:16:07 +0000

    inital commit

--- Go_vdotu/internal / handler/handler.go
+++ Go_vdotu/internal / handler/handler.go
@@ -5,13 +5,16 @@ import (
 	"net/http"
 )
 
+//
 type Handler struct {
 }
 
+//
 func NewHandler() *Handler {
 	return &Handler{}
 }
 
+//
 func (h *Handler) Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hello Golang Voronezh!")
 }
