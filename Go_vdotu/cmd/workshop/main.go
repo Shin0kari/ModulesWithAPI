dddcacf7fe029733b49eedaaa28108a993a037a1
Shinokari <kasper13122@mail.ru>
2021-09-18 18:06:20 +0000

    Add routing

--- Go_vdotu/cmd/workshop/main.go
+++ Go_vdotu/cmd/workshop/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+
+	"workshop/internal/handler"
+)
+
 func main() {
-	print()
+	h := handler.NewHandler()
+
+	r := chi.NewRouter()
+
+	r.Get("/hello", h.Hello)
+
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
 }
